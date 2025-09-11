412ff29a2e3d6857224b8c3c9d1e52385c7a9cb9
SumaBadugu <sumasrinivas0211@gmail.com>
2025-09-11 09:35:44 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(cors());
 app.use(express.static('public'));
 
 //connecting database
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
 
 db.connect(err =>{
     if(err){
